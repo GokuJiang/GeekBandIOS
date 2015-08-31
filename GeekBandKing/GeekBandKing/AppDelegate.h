1b76515d48c5f012e672bfac1f6bcecc8e080417
Json <775096422@qq.com>
2015-08-31 20:41:59 +0800

    init project

---
+++ GeekBandKing/GeekBandKing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeekBandKing
+//
+//  Created by Json on 15/8/31.
+//  Copyright (c) 2015年 Json. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeekBandKing/GeekBandKing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GeekBandKing
+//
+//  Created by Json on 15/8/31.
+//  Copyright (c) 2015年 Json. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
